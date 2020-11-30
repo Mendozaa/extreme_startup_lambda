60399f0aa88bca58be880a30b80618cbf5b0e05a
unknown <gersonmendozacarranza@gmail.com>
2020-11-30 20:58:39 +0000

    2 test responder model

--- src/test/java/com/serverless/ResponderModelTest.java
+++ src/test/java/com/serverless/ResponderModelTest.java
@@ -1,4 +1,29 @@
 package com.serverless;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ResponderModelTest {
+    ResponderModel responderModel = new ResponderModel();
+    @Test
+    public void cuandoNoExistePreguntaRetornaDevco(){
+        //Arrange
+        String question="";
+        //Act
+        String response = responderModel.answer(question);
+        //Assert
+        assertThat(response).isEqualTo("devco");
+
+    }
+
+    @Test
+    public void cuandoPreguntanPorSumar10y5yretorna15(){
+        //Arrange
+            String question="1234: what is the sum of 10 and 5";
+        //Act
+            String response=responderModel.answer(question);
+        //Assert
+            assertThat(response).isEqualTo("15");
+    }
 }
